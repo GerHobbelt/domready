c4aac428e6d31e9c9aef06020f909f40700ceb05
Victor Villaverde Laan <info@freelancephp.net>
2012-01-19 18:55:23 +0100

    Combined test of normal and minified version

--- test/index.php
+++ test/index.php
@@ -1,3 +1,4 @@
+<?php $testMinified = isset($_GET['minified']); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr" id="html">
 <head>
@@ -5,11 +6,17 @@
 	<title>DOMReady Test Suite</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="qunit/qunit.css" />
 	<script type="text/javascript" src="qunit/qunit.js"></script>
-	<script type="text/javascript" src="../src/domready.js"></script>
+	<script type="text/javascript" src="../src/domready<?php echo ( $testMinified ) ? '.min' : '' ?>.js"></script>
 	<script type="text/javascript" src="unit/domready.js"></script>
 </head>
 <body id="body">
-	<h1 id="qunit-header"><a href="/jquery/test/index.html">DOMReady Test Suite</a></h1>
+	<h1 id="qunit-header">
+		<a href="/jquery/test/index.html">DOMReady Test Suite</a>
+		<label>
+			<input type="checkbox" name="minified" <?php echo ( $testMinified ) ? 'checked="checked"' : '' ?> />
+			test minified version
+		</label>
+	</h1>
 	<h2 id="qunit-banner"></h2>
 	<div id="qunit-testrunner-toolbar"></div>
 	<h2 id="qunit-userAgent"></h2>
